d9e6b4e26fc306ad61eac7a01f54e3336df81ecc
xiaoyu <991607476@qq.com>
2019-10-28 18:20:28 +0800

    搭建框架

---
+++ BLOG-Server/Server-User/src/main/java/com/oddmy/blog/server/ServerUserApplication.java
@@ -0,0 +1,18 @@
+package com.oddmy.blog.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 项目启动类
+ *
+ * @author fish
+ * @date 2019/10/28 17:42
+ */
+@SpringBootApplication
+public class ServerUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerUserApplication.class, args);
+    }
+}
