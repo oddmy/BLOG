a5c64466a26b340e18cdd2bb2f6bd35f3c5e68ca
xiaoyu <991607476@qq.com>
2019-10-29 11:33:44 +0800

    搭建注册中心框架

--- BLOG-Server/Server-Manage/src/main/java/com/oddmy/blog/server/ManageApplication.java
+++ BLOG-Server/Server-Manage/src/main/java/com/oddmy/blog/server/ManageApplication.java
@@ -2,19 +2,19 @@ package com.oddmy.blog.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * TODO description
  *
  * @author fish
- * @date 2019/10/29 10:02
+ * @date 2019/10/29 10:58
  */
-@EnableDiscoveryClient
+@EnableEurekaServer
 @SpringBootApplication
-public class GateWayApplication {
+public class ManageApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(GateWayApplication.class, args);
+        SpringApplication.run(ManageApplication.class, args);
     }
 }
